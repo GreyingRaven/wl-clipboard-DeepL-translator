92b8670e16e1b7af257bcef7dc3147235a3fbf60
GreyingRaven <nno.connell@gmail.com>
2025-03-25 23:59:53 +0100

    Moved code so it only calls deepl api if there is an apikey. Commented code

--- translator.sh
+++ translator.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 
-history_file="$HOME/.last_translation"
+# Create config folder
+mkdir -p "$HOME/.config/wl-translator"
+# Set file varibles
+history_file="$HOME/.config/wl-translator/.last_translation"
+apikey_file="$HOME/.config/wl-translator/.deepl_apikey"
+# Set time exclusion regex
 time_regex='^.*[0-9]{2}[-:][0-9]{2}[-:][0-9]{2}.*$'
+# Check if there is something saved as a previous text to translate
 if [ -f "$history_file" ]; then
     last_translation=$(cat "$history_file")
 fi
-
-if [ -f "$HOME/.local/bin/.deepl_apikey" ]; then
-    API_KEY=$(cat "$HOME/.local/bin/.deepl_apikey")
-
-else
-    echo "No APIKEY for DeepL API found. Create a file .deepl_apikey next to this script"
-fi
-
-clear
-translate_text="$(wl-paste)"
-
-if [ ! -z "$translate_text" ] && [ "$translate_text" != "$last_translation" ] && ! [[ "$translate_text" =~ $time_regex ]]; then
-    echo $translate_text > "$history_file"
-    printf $translate_text "\n"
-    printf '\n
+# Check if there is an API key, only continue if there is
+if [ -f "$apikey_file" ]; then
+    API_KEY=$(cat "$apikey_file")
+    # Clean terminal from previous translations
+    clear
+    # Print original text
+    translate_text="$(wl-paste)"
+    # Check if the text to translate is:
+    ## Not empty
+    ## Not equal to last translation
+    ## Doesn't contain time
+    if [ ! -z "$translate_text" ] && [ "$translate_text" != "$last_translation" ] && ! [[ "$translate_text" =~ $time_regex ]]; then
+	echo $translate_text > "$history_file"
+	printf $translate_text "\n"
+	printf '\n
      -
     \n'
-    curl --silent -X POST https://api-free.deepl.com/v2/translate   --header "Content-Type: application/json"   --header "Authorization: DeepL-Auth-Key $API_KEY"   --data '
+	# Calls deepl free api and prints the translated text
+	curl --silent -X POST https://api-free.deepl.com/v2/translate   --header "Content-Type: application/json"   --header "Authorization: DeepL-Auth-Key $API_KEY"   --data '
       {
 	      "text": ["'$translate_text'"],
 	      "source_lang": "JA",
 	      "target_lang": "EN"
       }' | jq -r .translations[0].text
+    fi
+    
+else
+    echo "No APIKEY for DeepL API found. Create a file .deepl_apikey containing your apikey in the .config folder for this script $/.config/wl-translator"
 fi
+
+
