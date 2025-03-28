a6edaf3d1d0b48d4b692ceacd4076973b09a5f65
GreyingRaven <nno.connell@gmail.com>
2025-03-29 00:41:00 +0100

    See changelog[20250329] section in readme

--- README.md
+++ README.md
@@ -12,16 +12,59 @@ curl -X POST https://api-free.deepl.com/v2/translate   --header "Content-Type: a
 ```
 
 ## How to use
+`wl-paste -w(--watch) command` allows for the execution of a command each time wl-copy is updated. This allows for the script to run on each new `copy` and call DeepL for a new translation
 
+The script will create a config folder in `~/.config/wl-translator` containing default hidden files for normal use:
+- `.last_translation` Used to prevent translating the same string twice in a row
+- `.translations` Map of translations to reduce the number of calls to DeepL api, if it has already been translated it recovers translation from this file.
+**Important**: This folder must contain `.deepl_apikey` file with a valid DeepL api key.
+
+To run the script use the following command:
 ```sh
 wl-paste -w sh ~/wl-clipboard-DeepL-translator/translate.sh
 ```
+Options can be added at the end of the command:
+```sh
+wl-paste -w sh ~/wl-clipboard-DeepL-translator/translate.sh -f ~/Documents/translations/rus2eng.translations -s RU -t EN
+```
+#### Available options:
+ - h    Print help text.
+ - f    Set translations file. (Default: ~/.config/wl-translator/.trasnlations).
+        To disable translations map point to non existing file.
+ - s    Set source_lang (Default: JA).
+ - t    Set target_lang (Default: EN).
+ - T    Enable/Disable excludion flag (Default: Enabled)[true|false].
+ - R    Enable/Disable removal flag (Default: Enabled)[true|false].
 
-`wl-paste -w(--watch) command` allows for the execution of a command each time wl-copy is updated. This allows for the script to run on each new `copy` and call DeepL for a new translation
 
 ## To Do
-- Add a flag to enable/disable translations containing time
+- Editable history translations
+ - Allow input after translation to update?
+ - Auxiliary script to update saved translations?
 
 ## Changelog
-[20250326] Added options [-h|f|s|t|T] h shows help; f sets translations file; s sets source language; t sets target language; T not implemented yet.
-Added associative array stored in a translations file settable fith f option
+[20250329] Unimplemented functionality
+- Implemented exclude flag.
+ - Default true.
+ - If active stops translation if it includes excluding regexes:
+  - Excludes strings containing time [00:00:00]
+  - Excludes combat strings containing health/mana/zp values
+- Implemented remove flag.
+ - Default true.
+ - If active removes lines from translation string that include remove regexes:
+  - Removes money lines [####Lo]
+- Removed option T. 
+- Added options [E|R] 
+ - E sets exclude flag.
+ - R sets remove flag.
+- Updated How to use.
+- Formatted changelog for easier reading.
+
+[20250326] Configurable options
+- Added options [-h|f|s|t|T] 
+ - h shows help; 
+ - f sets translations file; 
+ - s sets source language; 
+ - t sets target language; 
+ - T not implemented yet.
+- Added associative array stored in a translations file settable fith f option
--- translator.sh
+++ translator.sh
@@ -8,12 +8,13 @@ Help()
     # Display Help
     echo "Translates last copied text in clipboard using DeepL API."
     echo
-    echo "Options: [-h|f|s|t|T]"
-    echo "h    Print this help text."
-    echo "f    Set translations file."
-    echo "s    Set source_lang (Default: JA)."
-    echo "t    Set target_lang (Default: EN)."
-    echo "T    Translate text containing time (Default: False)."
+    echo "Options: [h|f|s|t|T|R]"
+    echo "-h    Print this help text."
+    echo "-f    Set translations file. (Default: ~/.config/wl-translator/.trasnlations). To disable translations map point to non existing file."
+    echo "-s    Set source_lang (Default: JA)."
+    echo "-t    Set target_lang (Default: EN)."
+    echo "-T    Enable/Disable excludion flag (Default: Enabled)[true|false]."
+    echo "-R    Enable/Disable removal flag (Default: Enabled)[true|false]."
 }
 
 
@@ -24,21 +25,31 @@ Help()
 mkdir -p "$HOME/.config/wl-translator"
 # Set file varibles
 history_file="$HOME/.config/wl-translator/.last_translation"
+# Create history file
+touch "$history_file"
 translations_file="$HOME/.config/wl-translator/.translations"
 apikey_file="$HOME/.config/wl-translator/.deepl_apikey"
-# Set time exclusion regex
+# Set flags
+remove_flag=true
+exclude_flag=true
+# Set line removal regexes
+money_regex='/^.*[0-9]Lo.*$/d'
+# Set exclusion regexes
 time_regex='^.*[0-9]{2}[-:][0-9]{2}[-:][0-9]{2}.*$'
+combat_regex='^.*[0-9]ZP.*$'
 # Set default languages
 source_lang="JA"
 target_lang="EN"
 
 
-
 ###########################################################
 # Main program                                            #
 ###########################################################
-# Get the options
-while getopts :h:f:s:t:T: flag
+#----------------------------------------------------------
+# Get the options                                         #
+#----------------------------------------------------------
+
+while getopts :h:f:s:t:T:R: flag
 do
     case "${flag}" in
 	h) # display help
@@ -50,71 +61,91 @@ do
 	    source_lang=${OPTARG};;
 	t) # set target_lang
 	    target_lang=${OPTARG};;
-	T) # set flag for time condition
-	    echo "Option not implemented"
-	    exit;;
+	T) # set exclusion flag
+	    exclude_flah=${OPTARG};;
+	R) # set removal flag
+	    remove_flag=${OPTARG};;
 	\?) # Invalid option
 	    echo "Error: Invalid option"
 	    exit;;
     esac
 done
+
+#----------------------------------------------------------
+# Get text and check if translatable                      #
+#----------------------------------------------------------
+
+# Asign text to translate from clipboard
+translate_text="$(wl-paste)"
+# If exclusion_flag exit if any exclusion regex is true
+if $exclude_flag && ! [[ "$translate_text" =~ $time_regex ]] && [[ "$translate_text" =~ $combat_regex ]] ; then
+    exit
+fi
+# If remove_flag active remove removal regexes from translation string
+if $remove_flag ; then 
+    translate_text=$(sed $money_regex <<< "$translate_text")
+fi
 # Check if there is something saved as a previous text to translate
 if [ -f "$history_file" ]; then
     last_translation=$(cat "$history_file")
 fi
-# Declare translations associative array
+# Exit if the text to translate is:
+## empty
+## equal to last translation
+if [ ! -z "$translate_text" ] && [ "$translate_text" != "$last_translation" ] ; then
+    exit
+fi
+
+#----------------------------------------------------------
+# Get translation                                         #
+#----------------------------------------------------------
+
+# If translations file exists declare translations associative array
 if [ -f "$translations_file" ]; then
     declare -A translations
     source -- "$translations_file"
-fi
-# Asign text to translate from clipboard
-translate_text="$(wl-paste)"
-translationhex=$(hexdump -ve '1/1 "%02x"' <<< "$translate_text")
-if [[ -v translations[$translationhex] ]]; then
-    clear
-    echo "hex: $translationhex"
-    echo "$translate_text"
-    echo "-"
-    echo "${translations[$translationhex]}"
-    exit
+    translationhex=$(hexdump -ve '1/1 "%02x"' <<< "$translate_text")
+    if [[ -v translations[$translationhex] ]]; then
+	clear
+	echo "hex: $translationhex"
+	echo "$translate_text"
+	echo "-"
+	echo "${translations[$translationhex]}"
+	exit
+    fi
 fi
 # Check if there is an API key, only continue if there is
 if [ -f "$apikey_file" ]; then
     API_KEY=$(cat "$apikey_file")
 
-    # Check if the text to translate is:
-    ## Not empty
-    ## Not equal to last translation
-    ## Doesn't contain time
-    if [ ! -z "$translate_text" ] && [ "$translate_text" != "$last_translation" ] && ! [[ "$translate_text" =~ $time_regex ]]; then
-	# Clear terminal to remove previous translation
-	clear
-	# Update history with latest translation
-	echo $translate_text > "$history_file"
-	# TODO persist translation for later recovery
-	# Print response with following format
-	## Original text
-	## -
-	## Translation
-	echo "$source_lang -> $target_lang"
-	printf $translate_text
-	printf '\n
+    # Clear terminal to remove previous translation
+    clear
+    # Update history with latest translation
+    echo $translate_text > "$history_file"
+    # TODO persist translation for later recovery
+    # Print response with following format
+    ## Original text
+    ## -
+    ## Translation
+    echo "$source_lang -> $target_lang"
+    printf $translate_text
+    printf '\n
 -
     \n'
-	# Calls deepl free api and prints the translated text
-	deepl_response=$(curl --silent -X POST https://api-free.deepl.com/v2/translate   --header "Content-Type: application/json"   --header "Authorization: DeepL-Auth-Key $API_KEY"   --data '
-      {
-	      "text": ["'$translate_text'"],
-	      "source_lang": "'$source_lang'",
-	      "target_lang": "'$target_lang'"
-      }')
-	translation=$(jq -r ".translations[0].text" <<< "${deepl_response}")
-	echo "$translation"
-	# Save translation to associative array and persist in file
+    # Calls deepl free api and prints the translated text
+    deepl_response=$(curl --silent -X POST https://api-free.deepl.com/v2/translate   --header "Content-Type: application/json"   --header "Authorization: DeepL-Auth-Key $API_KEY"   --data '
+     {
+      "text": ["'$translate_text'"],
+      "source_lang": "'$source_lang'",
+      "target_lang": "'$target_lang'"
+     }')
+    translation=$(jq -r ".translations[0].text" <<< "${deepl_response}")
+    echo "$translation"
+    # If translations file exists save translation to associative array and persist in file
+    if [ -f "$translations_file" ]; then
 	translations["$translationhex"]="$translation"
 	declare -p translations > "$translations_file"
     fi
-    
 else
     echo "No APIKEY for DeepL API found. Create a file .deepl_apikey containing your apikey in the .config folder for this script $/.config/wl-translator"
 fi
